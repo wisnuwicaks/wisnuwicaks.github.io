cbc04fbe7a8e926df5260ed502b8a767e3773aac
wisnuwicaks <32096881+wisnuwicaks@users.noreply.github.com>
2020-04-12 17:14:21 +0700

    Updates

---
+++ precache-manifest.e86aba9e4f848291566a521a377f3566.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc1f80cfd6782e1084b94d9ce2930655",
+    "url": "/BelajarReact/index.html"
+  },
+  {
+    "revision": "dc10fe118f28f1caadc8",
+    "url": "/BelajarReact/static/css/main.164ccbeb.chunk.css"
+  },
+  {
+    "revision": "f8790ed26e2ece3b46c3",
+    "url": "/BelajarReact/static/js/2.f64598ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BelajarReact/static/js/2.f64598ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc10fe118f28f1caadc8",
+    "url": "/BelajarReact/static/js/main.292d6bc0.chunk.js"
+  },
+  {
+    "revision": "af196ac065b45875922b",
+    "url": "/BelajarReact/static/js/runtime-main.49a68d99.js"
+  },
+  {
+    "revision": "292fa2623b5c723233dd51b573a1d950",
+    "url": "/BelajarReact/static/media/Brave.292fa262.png"
+  },
+  {
+    "revision": "966a3385df68f96738b9fd7f36bc9ee3",
+    "url": "/BelajarReact/static/media/Crazy.966a3385.png"
+  },
+  {
+    "revision": "913352208bf401ffcdfc5a02feace50c",
+    "url": "/BelajarReact/static/media/Educated.91335220.png"
+  },
+  {
+    "revision": "6cc4ec42436e9b024bbf57a9bef27c9f",
+    "url": "/BelajarReact/static/media/Handmaid.6cc4ec42.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/BelajarReact/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wisnuwicaks/BelajarReact/precache-manifest.31f1ba07a9db550c7d05976570a86b70.js"
+  "/BelajarReact/precache-manifest.e86aba9e4f848291566a521a377f3566.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wisnuwicaks/BelajarReact/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BelajarReact/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
